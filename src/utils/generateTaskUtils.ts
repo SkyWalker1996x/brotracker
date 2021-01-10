2cb6495787408e70d6f93c910788aeab51769559
Chekhuta Dmytro <chehuta1996@gmail.com>
2021-01-10 23:26:50 +0200

    feat: create generateTask utils

---
+++ src/utils/generateTaskUtils.ts
@@ -0,0 +1,38 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export const getRandomNumber = (min: number, max: number) => {
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+};
+
+export const generateTasks = () => {
+    const randomNumbers = getRandomNumber(10, 15);
+
+    const todayInMS = Date.now();
+    const todayDate = new Date(todayInMS);
+
+    let timeStart;
+    let timeEnd = new Date(
+        todayDate.getFullYear(),
+        todayDate.getMonth(),
+        todayDate.getDate(),
+        8,
+        0,
+        0
+    ).getTime();
+    let id;
+
+    return Array.from({ length: randomNumbers }, () => {
+        timeStart = timeEnd + getRandomNumber(300000, 900000);
+        timeEnd = timeStart + getRandomNumber(600000, 5400000);
+        id = uuidv4();
+
+        return {
+            id,
+            taskName: `Task ${id.substr(0, 8)}`,
+            timeStart,
+            timeEnd,
+            timeSpend: timeEnd - timeStart,
+        };
+    });
+};
