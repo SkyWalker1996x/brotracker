c345820cd102ed660f164c993f0389209a30ba80
Chekhuta Dmytro <chehuta1996@gmail.com>
2021-01-15 01:50:21 +0200

    refactor: create additional rules for save to localStorage

--- src/store/sagas/activateTimerSaga.ts
+++ src/store/sagas/activateTimerSaga.ts
@@ -2,7 +2,9 @@ import { put, select, takeEvery } from 'redux-saga/effects';
 import { ACTIVATE_TIMER } from '../types';
 import { startTask } from '../actions/currentTaskActions';
 import { State } from '../../interfaces/Store';
+import { saveToLocalStorage } from '../../utils/localStorageUtils';
 
+const getState = (state: State) => state;
 const getTaskTimeStart = (state: State) => state.currentTask.timeStart;
 
 function* workerActivateTimer() {
@@ -10,6 +12,8 @@ function* workerActivateTimer() {
   if (taskTimeStart === 0) {
     yield put(startTask());
   }
+  const state = yield select(getState);
+  saveToLocalStorage(state);
 }
 
 export function* watcherActivateTimer() {
