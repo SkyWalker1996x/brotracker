d5639ecdfca2a03521de8726f3b401037648c64a
Chekhuta Dmytro <chehuta1996@gmail.com>
2021-01-14 22:42:17 +0200

    refactor: spread TimerInfo component to Timer component

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 // components
-import ColumnWrapper from '../../UI/wrappers/ColumnWrapper/ColumnWrapper';
 import TaskInput from './TaskInput/TaskInput';
-import TimerInfo from './TimerInfo/TimerInfo';
 import TimerButton from './TimerButton/TimerButton';
+// utils
+import { extractSpendTime } from '../../utils/timesManipulationUtils';
+// interfaces
+import { State } from '../../interfaces/Store';
+// styles
+import './styles.css';
 
 const Timer = () => {
+  const timeSpend = useSelector((state: State) => state.currentTask.timeSpend);
+
   return (
-    <ColumnWrapper>
+    <div className="column-wrapper">
       <TaskInput />
-      <TimerInfo />
+      <div className="circle-wrapper">{extractSpendTime(timeSpend)}</div>
       <TimerButton />
-    </ColumnWrapper>
+    </div>
   );
 };
 
