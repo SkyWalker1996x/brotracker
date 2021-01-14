27103507e48282e19ef3bdf475ba57e40a54c9c7
Chekhuta Dmytro <chehuta1996@gmail.com>
2021-01-14 23:11:59 +0200

    refactor: spread TaskButton component to Timer component

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import TimerButton from './TimerButton/TimerButton';
 // MUI-components
 import TextField from '@material-ui/core/TextField';
+import { Button } from '@material-ui/core';
 // actions
 import { setTaskName } from '../../store/actions/currentTaskActions';
+import { showWarning } from '../../store/actions/emptyWarningActions';
+import {
+  activateTimer,
+  inactivateTimer,
+} from '../../store/actions/activeTimerActions';
 // utils
 import { extractSpendTime } from '../../utils/timesManipulationUtils';
 // interfaces
@@ -12,12 +17,20 @@ import { State } from '../../interfaces/Store';
 // styles
 import './mui-styles.css';
 import './styles.css';
+import { useStyles } from './mui-styles';
 
 const Timer = () => {
-  const currentTask = useSelector((state: State) => state.currentTask);
+  const classes = useStyles();
   const dispatch = useDispatch();
+  const currentTask = useSelector((state: State) => state.currentTask);
+  const activeTimer = useSelector((state: State) => state.activeTimer);
 
   const { taskName, timeSpend } = currentTask;
+
+  const inactivateListener =
+    taskName.trim() === '' ? showWarning : inactivateTimer;
+
+  const listener = activeTimer ? inactivateListener : activateTimer;
 
   return (
     <div className="column-wrapper">
@@ -28,7 +41,13 @@ const Timer = () => {
         value={taskName}
       />
       <div className="circle-wrapper">{extractSpendTime(timeSpend)}</div>
-      <TimerButton />
+      <Button
+        variant="contained"
+        className={classes.timerButton}
+        onClick={() => dispatch(listener())}
+      >
+        {activeTimer ? 'Stop' : 'Start'}
+      </Button>
     </div>
   );
 };
