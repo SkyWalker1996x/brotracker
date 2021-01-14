cc798004de087a9cb9b27be01888ac23c89884e6
Chekhuta Dmytro <chehuta1996@gmail.com>
2021-01-14 22:50:54 +0200

    refactor: spread TaskInput component to Timer component

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,21 +1,32 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-// components
-import TaskInput from './TaskInput/TaskInput';
+import { useDispatch, useSelector } from 'react-redux';
 import TimerButton from './TimerButton/TimerButton';
+// MUI-components
+import TextField from '@material-ui/core/TextField';
+// actions
+import { setTaskName } from '../../store/actions/currentTaskActions';
 // utils
 import { extractSpendTime } from '../../utils/timesManipulationUtils';
 // interfaces
 import { State } from '../../interfaces/Store';
 // styles
+import './mui-styles.css';
 import './styles.css';
 
 const Timer = () => {
-  const timeSpend = useSelector((state: State) => state.currentTask.timeSpend);
+  const currentTask = useSelector((state: State) => state.currentTask);
+  const dispatch = useDispatch();
+
+  const { taskName, timeSpend } = currentTask;
 
   return (
     <div className="column-wrapper">
-      <TaskInput />
+      <TextField
+        id="standard-basic"
+        label="Name of your task"
+        onChange={(e) => dispatch(setTaskName(e.target.value))}
+        value={taskName}
+      />
       <div className="circle-wrapper">{extractSpendTime(timeSpend)}</div>
       <TimerButton />
     </div>
